492b4af077b5676a7e9595e980a2b29cef1c92e1
jiwanlal <89865194+jiwanlal@users.noreply.github.com>
2023-07-13 20:22:54 +0530

    left side menu change (#26)
    * browse file design
    
    * table ui issue fixed
    
    * table ui change
    
    * Collapse able according  next perv done
    
    * labor routing creation
    
    * project lobar page created
    
    * Add Element remove
    
    * project labor design
    
    * left side bar change
    
    ---------
    
    Co-authored-by: jiwannishad <jiwan@leadangel.com>

--- src/app/common/components/layout/side-nav/side-nav.component.ts
+++ src/app/common/components/layout/side-nav/side-nav.component.ts
@@ -1,4 +1,10 @@
-import { Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Input,
+  Inject,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  OnDestroy} from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { routerModel } from 'src/app/common/models/router.model';
 
@@ -10,10 +16,85 @@ import { routerModel } from 'src/app/common/models/router.model';
 export class SideNavComponent implements OnInit {
 
   @Input() routers: routerModel[];
-
-  constructor() { }
+ 
+  public innerHeight: any;
+  public bodyTag: any;
+  listMaxHeight: string;
+  listMaxWidth: string;
+  userFullName: string;
+  userImg: string;
+  userType: string;
+  headerHeight = 60;
+  currentRoute: string;
+  routerObj = null;
+  constructor(  @Inject(DOCUMENT) private document: Document,
+  private renderer: Renderer2,
+  public elementRef: ElementRef) { }
 
   ngOnInit(): void {
   }
+  initLeftSidebar() {
+    const _this = this;
+    // Set menu height
+    _this.setMenuHeight();
+    _this.checkStatuForResize(true);
+  }
+  setMenuHeight() {
+    this.innerHeight = window.innerHeight;
+    const height = this.innerHeight - this.headerHeight;
+    this.listMaxHeight = height + "";
+    this.listMaxWidth = "500px";
+  }
+  isOpen() {
+    return this.bodyTag.classList.contains("overlay-open");
+  }
+  checkStatuForResize(firstTime) {
+    if (window.innerWidth < 1170) {
+      this.renderer.addClass(this.document.body, "ls-closed");
+    } else {
+      this.renderer.removeClass(this.document.body, "ls-closed");
+    }
+  }
+  mouseHover(e) {
+    const body = this.elementRef.nativeElement.closest("body");
+    if (body.classList.contains("submenu-closed")) {
+      this.renderer.addClass(this.document.body, "side-closed-hover");
+      this.renderer.removeClass(this.document.body, "submenu-closed");
+    }
+  }
+  mouseOut(e) {
+    const body = this.elementRef.nativeElement.closest("body");
+    if (body.classList.contains("side-closed-hover")) {
+      this.renderer.removeClass(this.document.body, "side-closed-hover");
+      this.renderer.addClass(this.document.body, "submenu-closed");
+    }
+  }
+  @HostListener("window:resize", ["$event"])
+  windowResizecall(event) {
+    this.setMenuHeight();
+    this.checkStatuForResize(false);
+  }
+  
+  @HostListener("document:mousedown", ["$event"])
 
+  onGlobalClick(event): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.renderer.removeClass(this.document.body, "overlay-open");
+    }
+  }
+  callToggleMenu(event: any, length: any) {
+    console.log(length)
+    if (length > 0) {
+      const parentElement = event.target.closest("li");
+      const activeClass = parentElement.classList.contains("active");
+
+      if (activeClass) {
+        this.renderer.removeClass(parentElement, "active");
+      } else {
+        this.renderer.addClass(parentElement, "active");
+      }
+    }
+  }
 }
+
+
--- src/app/components/authorized/authorized.component.ts
+++ src/app/components/authorized/authorized.component.ts
@@ -14,71 +14,146 @@ export class AuthorizedComponent implements OnInit, AfterContentInit {
   public loading$: Observable<boolean>;
 
   public routerData: routerModel[] = [
-    {
-      label: "Dashboard",
-      route: "dashboard",
-      icon: "dashboard",
-    },
-    {
-      label: "Project",
-      route: "project",
-      icon: "assignment",
+    {"path": "dashboard",
+"title": "Dashboard",
+"iconType": "material-icons-two-tone",
+"icon": "dashboard",
+"class": "",
+"groupTitle": false,
+"badge": "",
+"badgeClass": "",
+"submenu":[]
+ },
+ {"path": "project",
+"title": "Project",
+"iconType": "material-icons-two-tone",
+"icon": "assignment",
+"class": "",
+"groupTitle": false,
+"badge": "",
+"badgeClass": "",
+"submenu":[]
+ },
+ {"path": "financial",
+ "title": "Financial",
+ "iconType": "material-icons-two-tone",
+ "icon": "card_travel",
+ "class": "",
+ "groupTitle": false,
+ "badge": "",
+ "badgeClass": "",
+ "submenu":[]
+  },
+  {"path": "report",
+ "title": "Report",
+ "iconType": "material-icons-two-tone",
+ "icon": "assessment",
+ "class": "",
+ "groupTitle": false,
+ "badge": "",
+ "badgeClass": "",
+ "submenu":[]
+  },
+  {"path": "labor",
+  "title": "Labor",
+  "iconType": "material-icons-two-tone",
+  "icon": "settings_accessibility",
+  "class": "menu-toggle",
+  "groupTitle": false,
+  "badge": "",
+  "badgeClass": "",
+  "submenu":[
+    {"path": "labor/labor-rate",
+  "title": "Labor Rate",
+  "iconType": "material-icons-two-tone",
+  "icon": "settings_accessibility",
+  "class": "",
+  "groupTitle": false,
+  "badge": "",
+  "badgeClass": "",
+  "submenu":[
+    
+  ]
+   },
+   {"path": "labor/project-labor",
+   "title": "Project Labor",
+   "iconType": "material-icons-two-tone",
+   "icon": "settings_accessibility",
+   "class": "",
+   "groupTitle": false,
+   "badge": "",
+   "badgeClass": "",
+   "submenu":[
+     
+   ]
+    }
+  ]
+   }
+//     {
+//       label: "Dashboard",
+//       route: "dashboard",
+//       icon: "dashboard",
+//     },
+//     {
+//       label: "Project",
+//       route: "project",
+//       icon: "assignment",
       
       
-    },
-    {
-      label: "Financial",
-      route: "financial",
-      icon: "card_travel",
-    },
-    {
-      label: "Report",
-      route: "report",
-      icon: "assessment",
-    },
-    {
-      label: "Labor",
-      route: "labor",
-      icon: "settings_accessibility",
-      children:[
-        {
-          label: "Labor Rate",
-          route: "labor/labor-rate",
-          icon: "settings_accessibility",
+//     },
+//     {
+//       label: "Financial",
+//       route: "financial",
+//       icon: "card_travel",
+//     },
+//     {
+//       label: "Report",
+//       route: "report",
+//       icon: "assessment",
+//     },
+//     {
+//       label: "Labor",
+//       route: "labor",
+//       icon: "settings_accessibility",
+//       children:[
+//         {
+//           label: "Labor Rate",
+//           route: "labor/labor-rate",
+//           icon: "settings_accessibility",
          
          
-        },
-        {
-          label: "Project Labor",
-          route: "labor/project-labor",
-          icon: "settings_accessibility",
+//         },
+//         {
+//           label: "Project Labor",
+//           route: "labor/project-labor",
+//           icon: "settings_accessibility",
           
          
-        },
-      ],
+//         },
+//       ],
       
      
-    },
-    {
-      label: "Material",
-      route: "",
-      icon: "precision_manufacturing",
-    },
-    {
-      label: "Setting",
-      route: "setting",
-      icon: "settings",
-    },
-    {
-      label: "User",
-      route: "user",
-      icon: "people",
-    },
-    {
-      label: "Voucher",
-      route: "voucher",
-      icon: "local_offer",
-    }
+//     },
+//     {
+//       label: "Material",
+//       route: "",
+//       icon: "precision_manufacturing",
+//     },
+//     {
+//       label: "Setting",
+//       route: "setting",
+//       icon: "settings",
+//     },
+//     {
+//       label: "User",
+//       route: "user",
+//       icon: "people",
+//     },
+//     {
+//       label: "Voucher",
+//       route: "voucher",
+//       icon: "local_offer",
+//     }
   ]
 
   constructor(public loader: LoadingService) { }
