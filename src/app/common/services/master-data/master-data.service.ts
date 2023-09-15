7bf1364169c66d800dff9265cee072e0cba613d4
jiwanlal <89865194+jiwanlal@users.noreply.github.com>
2023-09-15 11:47:07 +0530

    url change (#39)
    * update changes
    
    * Delete Draft
    
    * Summry page design
    
    * attachment file change
    
    * Update attachment-details.component.ts
    
    * url change
    
    ---------
    
    Co-authored-by: jiwannishad <jiwan@leadangel.com>

--- src/app/common/services/master-data/master-data.service.ts
+++ src/app/common/services/master-data/master-data.service.ts
@@ -17,7 +17,7 @@ export class MasterDataService extends BaseService {
   constructor(public http: HttpClient,
     public controllers: Controllers,
     public config: ConfigService<IConfig>) {
-    super(http, 'http://localhost:1610')
+    super(http, 'http://fpmsind.com:1611')
     //"APIUrl Dev": "http://3.110.148.123:1610"
     //"APIUrl Pord": "http://13.234.115.22:1611"
   }
